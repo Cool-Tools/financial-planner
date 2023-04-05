a31eb2287d55a071c44723890e9cb85b209673ca
Steve Kay <91057102+SteveKay-Methods@users.noreply.github.com>
2023-04-05 11:43:26 +0100

    FP-6: Stash for in progress

---
+++ FP.Models/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace FP.Models;
+
+public class Address
+{
+    public string? HouseNameOrNumber { get; set; }
+    public List<string>? AddressLine { get; set; }
+    public string? PostcodeOrZip { get; set; }
+}
---
+++ FP.Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using FP.Models.Base;
+
+namespace FP.Models;
+
+public class Tag : BaseObject
+{
+    public string? Name { get; set; }
+}
+
+public class TagCollection : List<Tag>
+{
+}
