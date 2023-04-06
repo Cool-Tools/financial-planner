c467b7a2796ec61b8d0e42590f4bb242edc158a6
Steve Kay <91057102+SteveKay-Methods@users.noreply.github.com>
2023-04-06 11:34:42 +0100

    FP-7: updates

--- FP.Models/Scheduling/Schedule.cs
+++ FP.Models/Scheduling/Schedule.cs
@@ -7,5 +7,6 @@ public class Schedule
     public DateOnly Date { get; set; }
     public ScheduleType Type { get; set; }
     public SchedulingExceptionRulesCollection? SchedulingRules { get; set; }
+    public int Every { get; set; } = 1; // describes how often (every 1 month, 2 weeks, etc)
 }
 
