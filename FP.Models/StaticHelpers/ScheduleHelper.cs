ce9156172c7b0e5589adbd3cc83ea695a48e33cb
Steve Kay <91057102+SteveKay-Methods@users.noreply.github.com>
2023-04-06 13:19:47 +0100

    [FP-7] - Added ToSchedulePrompt static helper

---
+++ FP.Models/StaticHelpers/ScheduleHelper.cs
@@ -0,0 +1,17 @@
+﻿using FP.Models.Enumerations;
+
+namespace FP.Models.StaticHelpers;
+
+public class ScheduleHelper
+{
+    public static string ToSchedulePrompt(ScheduleType scheduleType) => scheduleType switch
+    {
+        ScheduleType.Once => "Once",
+        ScheduleType.Annually => "Year(s)",
+        ScheduleType.Monthly => "Month(s)",
+        ScheduleType.Daily  => "Day(s)",
+        ScheduleType.Weekly => "Week(s)",
+        ScheduleType.Custom => "Custom",
+        _ => throw new ArgumentOutOfRangeException(nameof(scheduleType), $"Unexpected scheduleType value: {scheduleType}")
+    };
+}
