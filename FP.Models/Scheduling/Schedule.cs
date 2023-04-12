5583104020dd5fbd42b32434a634cc73bf9d550d
Steve Kay <stevekay72@gmail.com>
2023-04-12 16:35:09 +0100

    [FP-9] - Fixed Schedule display for date picker and multi select (days)

--- FP.Models/Scheduling/Schedule.cs
+++ FP.Models/Scheduling/Schedule.cs
@@ -4,9 +4,10 @@ namespace FP.Models.Scheduling;
 
 public class Schedule
 {
-    public DateOnly Date { get; set; }  //  The date of the first (or only) transaction
+    public DateTime? Date { get; set; }  //  The date of the first (or only) transaction
     public ScheduleType Type { get; set; }  //  How often this transaction repeats
     public SchedulingExceptionRulesCollection? SchedulingRules { get; set; }
     public int Every { get; set; } = 1; // describes how often (every 1 month, 2 weeks, etc)
+    public IEnumerable<string>? DaysOfWeek { get; set; }
 }
 
