0d6a06b7775f0a62861f982f1346f7bb597c3689
Steve Kay <stevekay72@gmail.com>
2023-05-10 00:40:54 +0100

    [FP-9] - updates

--- FP.Models/Scheduling/Schedule.cs
+++ FP.Models/Scheduling/Schedule.cs
@@ -5,7 +5,8 @@ namespace FP.Models.Scheduling;
 public class Schedule
 {
     public DateTime? StartDate { get; set; }  //  The date of the first (or only) transaction
-    public DateTime? EndDate { get; set; }  //  The date of the first (or only) transaction
+    public int RepeatCount { get; set; }    //  How many times the schedule repeats
+    public DateTime? EndDate { get; set; }  //  The date of the last transaction if end date is selected
     public ScheduleType Type { get; set; }  //  How often this transaction repeats
     public SchedulingExceptionRulesCollection? SchedulingRules { get; set; }
     public int Every { get; set; } = 1; // describes how often (every 1 month, 2 weeks, etc)
